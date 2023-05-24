1801b52fa2892257affab728e678aa13af0870f1
lendihop <mishin.danil2017@gmail.com>
2023-05-24 23:14:05 +0200

    base structure

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,47 @@ import './App.css';
 const App = () => {
   return (
     <div className="app">
-      Clothing app
+      <div className="categories-container">
+        <div className="category-container">
+          {/*<img/>*/}
+          <div className="category-body-container">
+            <h2>Hats</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+
+        <div className="category-container">
+          {/*<img/>*/}
+          <div className="category-body-container">
+            <h2>Jackets</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+
+        <div className="category-container">
+          {/*<img/>*/}
+          <div className="category-body-container">
+            <h2>Sneakers</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+
+        <div className="category-container">
+          {/*<img/>*/}
+          <div className="category-body-container">
+            <h2>Womens</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+
+        <div className="category-container">
+          {/*<img/>*/}
+          <div className="category-body-container">
+            <h2>Mans</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
